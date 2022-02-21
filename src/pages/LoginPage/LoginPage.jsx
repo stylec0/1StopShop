64a736e4335ba90693dcf42ee1ec2d89af012138
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2022-02-21 12:59:56 -0800

    Added login pages and signup pages, 1:00pm 2/21/22

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,18 +2,114 @@ import React, { useState } from "react";
 import "./LoginPage.css";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import userService from "../../utils/userService";
+import { useNavigate, Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
 
 export default function LoginPage(props) {
+  const [error, setError] = useState("");
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await userService.login(state);
+      // Route to wherever you want!
+      props.handleSignUpOrLogin();
+      navigate("/");
+    } catch (err) {
+      // Invalid user data (probably duplicate email)
+      setError(err.message);
+    }
+  }
+
   return (
     <>
-      <h1>Setup Login Page</h1>
-      <ul>
-        <li>Read the Login Model, You can change it to fit your needs</li>
-        <li>
-          Make sure you read the Login Controller, to know how it is setup to
-          find the user!
-        </li>
-      </ul>
+      <Grid
+        textAlign="center"
+       
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" color="teal" textAlign="center">
+            <Image src="https://i.imgur.com/s4LrnlU.png" /> Log-in to your
+            account
+          </Header>
+          <Form autoComplete="off" onSubmit={handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                type="email"
+                name="email"
+                placeholder="email"
+                value={state.email}
+                onChange={handleChange}
+                required
+              />
+              <Form.Input
+                name="password"
+                type="password"
+                placeholder="password"
+                value={state.password}
+                onChange={handleChange}
+                required
+              />
+              <Button
+                color="teal"
+                fluid
+                size="large"
+                type="submit"
+                className="btn"
+              >
+                Login
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            New to us? <Link to="/signup">Sign Up</Link>
+          </Message>
+          {error ? <ErrorMessage error={error} /> : null}
+        </Grid.Column>
+      </Grid>
     </>
   );
 }
+
+//import React, { useState } from "react";
+//import "./LoginPage.css";
+//import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+//import userService from "../../utils/userService";
+
+//export default function LoginPage(props) {
+//  return (
+//    <>
+//      <h1>Setup Login Page</h1>
+//      <ul>
+//        <li>Read the Login Model, You can change it to fit your needs</li>
+//        <li>
+//          Make sure you read the Login Controller, to know how it is setup to
+//          find the user!
+//        </li>
+//      </ul>
+//    </>
+//  );
+//}
